d76a19a42c8d6ce85dce7ee9f27e024ce6e76aa6
YeongRoYun <58219267+YeongRoYun@users.noreply.github.com>
2023-12-10 13:54:55 +0900

    [feat] brands

---
+++ database/data.sql
@@ -0,0 +1,11 @@
+# Brand
+INSERT INTO brands(id, slug, name, image)
+VALUES(1, "gs25", "GS25", "https://image.pyoniverse.kr/brands/gs25-logo.webp");
+INSERT INTO brands(id, slug, name, image)
+VALUES(2, "cu", "CU", "https://image.pyoniverse.kr/brands/cu-logo.webp");
+INSERT INTO brands(id, slug, name, image)
+VALUES(3, "seven-eleven", "7-Eleven", "https://image.pyoniverse.kr/brands/seveneleven-logo.webp");
+INSERT INTO brands(id, slug, name, image)
+VALUES(4, "emart24", "Emart24", "https://image.pyoniverse.kr/brands/emart24-logo.webp");
+INSERT INTO brands(id, slug, name, image)
+VALUES(5, "c-space", "CSpace", "https://image.pyoniverse.kr/brands/cspace-logo.webp");
