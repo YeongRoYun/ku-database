0e762afc3ce4bd70837e3c813baf97a66ea01128
YeongRoYun <58219267+YeongRoYun@users.noreply.github.com>
2023-12-14 14:45:33 +0900

    [feat] auth login

---
+++ app/business/ProductBusiness.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app\business;
+require_once $_SERVER["DOCUMENT_ROOT"] . "/app/interface/Business.php";
+
+use app\interface\Business;
+
+class ProductBusiness implements Business
+{
+
+}
